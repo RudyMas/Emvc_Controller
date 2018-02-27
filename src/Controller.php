0030c947ca78cde358bd84ee1ca7c499ed2367cd
Rudy Mas <rudy.mas@rudymas.be>
2018-02-27 20:14:01 +0100

    Added rudymas/html5

--- src/Controller.php
+++ src/Controller.php
@@ -13,8 +13,8 @@ use Twig_Loader_Filesystem;
  * @author      Rudy Mas <rudy.mas@rmsoft.be>
  * @copyright   2016-2018, rmsoft.be. (http://www.rmsoft.be/)
  * @license     https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version     1.4.2
- * @package     Library
+ * @version     1.4.3
+ * @package     EasyMVC\Controller
  */
 class Controller
 {
