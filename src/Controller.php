93d4c4c45988e86e75e2c2f06fbe09377711c5e1
Rudy Mas <rudy.mas@rudymas.be>
2018-07-12 17:56:31 +0200

    Fixed version numbering

--- src/Controller.php
+++ src/Controller.php
@@ -13,7 +13,7 @@ use Twig_Loader_Filesystem;
  * @author      Rudy Mas <rudy.mas@rmsoft.be>
  * @copyright   2016-2018, rmsoft.be. (http://www.rmsoft.be/)
  * @license     https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version     1.5.2
+ * @version     1.5.2.17
  * @package     EasyMVC\Controller
  */
 class Controller
