0867f531be6a47d4482c61a394a727dcfe060a92
Rudy Mas <rudy.mas@rudymas.be>
2022-04-06 22:38:24 +0200

    Updated to PHP 7.4

--- src/Controller.php
+++ src/Controller.php
@@ -12,12 +12,12 @@ use Twig\Extension\DebugExtension;
 use Twig\Loader\FilesystemLoader;
 
 /**
- * Class Controller (PHP version 7.2)
+ * Class Controller (PHP version 7.4)
  *
  * @author      Rudy Mas <rudy.mas@rmsoft.be>
- * @copyright   2016-2020, rmsoft.be. (http://www.rmsoft.be/)
+ * @copyright   2016-2022, rmsoft.be. (http://www.rmsoft.be/)
  * @license     https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version     1.5.6.0
+ * @version     1.5.6.1
  * @package     EasyMVC\Controller
  */
 class Controller
@@ -90,7 +90,7 @@ class Controller
      */
     private function renderJSON(array $data, int $httpResponseCode = 200): void
     {
-        if ($httpResponseCode >= 200 && $httpResponseCode <= 206) {
+        if ($httpResponseCode >= 200 && $httpResponseCode <= 299) {
             $jsonData = $data;
         } else {
             $jsonData['error']['code'] = $httpResponseCode;
